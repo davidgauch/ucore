a908179d6d458f31d3021091d5526091b156b934
David Gauch <davidgauch@gmail.com>
2026-08-29 15:46:51 -0700

    Initial commit

---
+++ build_files/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Copy the contents of system_files/ of the git repo to /
+cp -avf "/ctx/system_files"/. /
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# Add mullvad-vpn
+mkdir /usr/lib/mullvad-vpn
+mkdir /var/opt
+ln -s /usr/lib/mullvad-vpn /opt/Mullvad\ VPN
+dnf5 config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
+dnf5 install -y mullvad-vpn
+# Fix broken `/opt/Mullvad VPN/` links
+rpm -ql mullvad-vpn | while read -r file; do
+    if [[ -L "$file" ]]; then
+        ln -sf "$(readlink -f "$file")" "$file"
+    elif [[ -f "$file" ]]; then
+        sed -i "s|/opt/Mullvad.*VPN/|/usr/lib/mullvad-vpn/|gw /dev/stdout" "$file"
+    fi
+done
+rm /opt/Mullvad\ VPN
+rmdir /var/opt
+
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
+
+#### Example for enabling a System Unit File
+
+# systemctl enable podman.socket
